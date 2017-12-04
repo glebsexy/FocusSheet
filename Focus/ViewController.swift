9c7ab0b21ed0b9442db3e3d6958b8cc1974d00d7
zyumbik <gleb.sabirzyanov@gmail.com>
2017-12-04 11:56:53 +1000

    Initial Commit

---
+++ Focus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Gleb Sabirzyanov on 04/12/2017.
+//  Copyright © 2017 Gleb Sabirzyanov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
